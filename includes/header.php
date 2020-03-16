59fc19635c04226714b6be8c60d211e8635c3cf8
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2020-03-16 11:00:36 -0400

    Starts site
    Signed-off-by: Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $pageName; ?> | darkeyes.xyz</title>
+		<meta name="title" content="<?php echo $pageName; ?> | darkeyes.xyz">
+		<meta name="description" content="">
+		<meta name="keywords" content="darkeyes, darkeyes.xyz, dark theme, darkness, dark, dark eyes, dark usercss, github">
+		<meta name="robots" content="index, follow">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="msapplication-TileColor" content="#8899a6">
+		<meta name="language" content="English">
+		<meta name="author" content="darkeyes.xyz Team">
+		<!-- Favicons -->
+		<link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
+		<link rel="manifest" href="/assets/img/site.webmanifest">
+		<link rel="mask-icon" href="/assets/img/safari-pinned-tab.svg" color="#8899a6">
+		<!-- Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Darker+Grotesque&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato&display=swap" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" />
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="/assets/css/styles.css" />
+		<!-- Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</head>
+
+	<body>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$pageName = 'Home';
+include('includes/header.php');
+?>
+
+<?php include('includes/footer.php'); ?>
